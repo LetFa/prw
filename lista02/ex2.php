5ec480772cae1872e92931ad01ce192676a58664
leticia <lefarias111@gmail.com>
2022-04-06 08:44:58 -0300

    ex2 php

--- lista02/ex2.php
+++ lista02/ex2.php
@@ -1,6 +1,6 @@
 <?php
-   $mes = '2'
-   switch($mes){
+   $mes = '2';
+   switch($mes) {
        case '1': echo 'Janeiro'; break;
        case '2': echo 'Fevereiro'; break;
        case '3': echo 'Marco'; break;
