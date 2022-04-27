76d74135ce073256ec11b429e18e28acd5e5fbdf
leticia <lefarias111@gmail.com>
2022-04-27 09:04:32 -0300

    projeto

--- projeto01/cadastro_usuario.php
+++ projeto01/cadastro_usuario.php
@@ -12,7 +12,8 @@
 
    $sql = " INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
     VALUES ('".$nome. "','".$email."','".$telefone."')";
-
+     echo'<br>';
+     echo'<br>';
     $result = mysqli_query($con, $sql);
     if($result)
       echo 'Dados inseridos com sucesso';
