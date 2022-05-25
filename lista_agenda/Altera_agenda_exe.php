98a5b309cbc0a7fbd1b32d08e884a1aad6393229
leticia <lefarias111@gmail.com>
2022-05-25 11:48:47 -0300

    projeto

--- lista_agenda/Altera_agenda_exe.php
+++ lista_agenda/Altera_agenda_exe.php
@@ -1,5 +1,24 @@
 <?php
-    include('conexao.php');
+
+include('conexao.php');
+
+// Upload da foto     
+$fotoNome = $_FILES['foto']['name'];
+$target_dir = "upload/";
+$target_file = $target_dir . basename($_FILES["foto"]["name"]);
+// Select file type
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Valid file extensions
+$extensions_arr = array("jpg","jpeg","png","gif");
+
+// Check extension
+if( in_array($imageFileType,$extensions_arr) ){      
+    // Upload file
+    if(move_uploaded_file($_FILES['foto']['tmp_name'],$target_dir.$fotoNome)){
+        $fotoBlob = addslashes(file_get_contents($target_dir.$fotoNome));
+    }
+}
+
     $id_agenda = $_POST['id_agenda'];
     $nome = $_POST['nome'];
     $apelido = $_POST['apelido'];
@@ -14,7 +33,8 @@
     echo "<h1> Alteração de dados </h1>";
     echo "<p> Nome Usuário: " . $nome . "<p>";    
 	
-	$sql = "UPDATE agenda SET
+    if(isset($fotoNome)){
+  	$sql = "UPDATE agenda SET
             nome='".$nome."',
             apelido='".$apelido."',
             endereco='".$endereco."',
@@ -24,7 +44,24 @@
             telefone='".$telefone."',
             celular='".$celular."',
             email='".$email."'
-          WHERE id_agenda=".$id_agenda;
+            foto_blob='".$fotoBlob."',
+            foto_nome='".$fotoNome."'
+          WHERE id_agenda=".$id_agenda;            
+          }
+          else
+          {
+            $sql = "UPDATE agenda SET
+                      nome='".$nome."',
+                      apelido='".$apelido."',
+                      endereco='".$endereco."',
+                      bairro='".$bairro."',
+                      cidade='".$cidade."',
+                      estado='".$estado."',
+                      telefone='".$telefone."',
+                      celular='".$celular."',
+                      email='".$email."' 
+                     WHERE id_agenda=".$id_agenda;
+          }
 	    
 	$result = mysqli_query($con, $sql);
 	if($result)
---
+++ projeto02/sql/exemplo_insert.sql
@@ -0,0 +1,6 @@
+USE projeto02;
+
+INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
+VALUES ('leticia Farias', 'lefarias111@mail.com', '9090-7070');
+
+SELECT * FROM usuario;
