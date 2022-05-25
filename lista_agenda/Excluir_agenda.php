cafcc185dbb520e0019aa9303361fb2c4543deed
leticia <lefarias111@gmail.com>
2022-05-25 09:19:27 -0300

    projeto

--- lista_agenda/Excluir_agenda.php
+++ lista_agenda/Excluir_agenda.php
@@ -15,3 +15,21 @@
   
 ?>
 <a href='listar_agenda.php'> Voltar</a>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<style>
+		body{
+			background-color: FireBrick;
+		}
+	</style>
+</head>
+<body>
+	
+</body>
+</html>
